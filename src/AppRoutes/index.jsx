5a571c8a2d3c8b82486f37f045037f12a1a3354d
MEHAKABDULLAH <mehakabdullah170@gmail.com>
2025-02-05 23:22:51 +0500

    changing

--- src/AppRoutes/index.jsx
+++ src/AppRoutes/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import {  HashRouter,BrowserRouter, Routes,Route } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
 import PostReady from '../pages/PostReady'
+import Posting from './Posting';
 
 
 const AppRouter = () => {
@@ -14,6 +15,7 @@ const AppRouter = () => {
           <Route path='/' element={<LoginPage/>}/>
           <Route path='/register' element={<RegisterPage/>}/>
           <Route path='/postready' element={<PostReady/>}/>
+          <Route path="/posting" component={Posting} />
 
 
   
